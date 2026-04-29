e5316230c3a9a6db51af9cf4f0cf90d751e265ec
josh <josh@UmbrellaTerminal.fritz.box>
2026-04-29 23:17:56 +0200

    - add editorconfig - add float terminal with header for global note

--- lua/light-notes.lua
+++ lua/light-notes.lua
@@ -1,1 +1,29 @@
-vim.print("We are in")
+vim.print("Loading light notes")
+local M = {}
+
+M.setup = function()
+  vim.print("Setup called")
+end
+
+M.open_global_note = function()
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+
+  local x = math.floor((vim.o.columns - width) / 2)
+  local y = math.floor((vim.o.lines - height) / 2)
+
+  local backing_buffer = vim.api.nvim_create_buf(false, true)
+
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = x,
+    row = y,
+    title = "Global Note",
+    title_pos = "center",
+  }
+  _ = vim.api.nvim_open_win(backing_buffer, true, win_config)
+end
+
+return M
